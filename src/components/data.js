14cae1147d21b1266f88e5fc04aabeee4e5771c1
JosueDeLosSantos <jdd1993@hotmail.com>
2023-09-18 13:53:21 -0400

    Initial commit

---
+++ src/components/data.js
@@ -0,0 +1,7 @@
+export const personalInfo = {
+    firsName: 'Josue',
+    lastName: 'De Los Santos',
+    email: 'jdd1993@hotmail.com',
+    ph: '1 809 419 3871',
+    location: 'Santo Domingo, DO'
+  }
